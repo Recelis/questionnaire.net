1583a38177fdfd9838d2fb02238cc77123ef2a4b
Recelis <jackymlui@live.com>
2025-06-18 22:02:44 +1000

    added actionresult on controllers
    - added singleton for the QuestionaireService (but should add it with the interface for inmemory and postgres)
    - wrote an inmemory questionnaire service but may not be necessary.

--- Services/QuestionnaireService.cs
+++ Services/QuestionnaireService.cs
@@ -1,17 +1,63 @@
 using LifeTracker.Models;
-using Microsoft.AspNetCore.Components;
 
 namespace LifeTracker.Services;
 
-public static class QuestionnaireService
+public class QuestionnaireService
 {
-    static List<Questionnaire> Questionnaires { get; } = new List<Questionnaire>();
-    static QuestionnaireService()
+    List<Questionnaire> _questionnaires { get; } = new List<Questionnaire>();
+    public QuestionnaireService()
     {
     }
 
-    public static List<Questionnaire> GetAll() => Questionnaires;
+    public List<Questionnaire> GetAll() => _questionnaires.ToList();
 
-    public static Questionnaire? Get(int QuestionnaireId) => Questionnaires.FirstOrDefault(x => x.Id == QuestionnaireId);
+    public Questionnaire? Get(int questionnaireId) => _questionnaires.FirstOrDefault(x => x.Id == questionnaireId);
 
+    public Questionnaire? Post(Questionnaire newQuestionnaire)
+    {
+        int newQuestionnaireId = newQuestionnaire.Id;
+        int index = _questionnaires.FindIndex(questionnaire => newQuestionnaireId == questionnaire.Id);
+        if (index == -1)
+        {
+            _questionnaires.Add(newQuestionnaire);
+            return newQuestionnaire;
+        }
+        else
+        {
+            return null;
+        }
+    }
+
+    public Questionnaire? Put(Questionnaire newQuestionnaire)
+    {
+        int newQuestionnaireId = newQuestionnaire.Id;
+        int index = _questionnaires.FindIndex(questionnaire => newQuestionnaireId == questionnaire.Id);
+        if (index > -1)
+        {
+            _questionnaires[index] = newQuestionnaire;
+            return newQuestionnaire;
+        }
+        else
+        {
+            Console.WriteLine("Failed to find the questionnaire");
+            return null;
+        }
+
+
+    }
+
+    public bool Delete(int questionnaireId)
+    {
+        Questionnaire? questionnaire = _questionnaires.Find(questionnaire => questionnaire.Id == questionnaireId);
+        if (questionnaire != null)
+        {
+            _questionnaires.Remove(questionnaire);
+            return true;
+        }
+        else
+        {
+            Console.WriteLine("Failed to find the questionnaire");
+            return false;
+        }
+    }
 }

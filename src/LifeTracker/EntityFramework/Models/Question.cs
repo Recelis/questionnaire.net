4a710b211a671dbe0e384b6bc2ee9e671ed00dc3
Recelis <jackymlui@live.com>
2025-07-21 06:51:13 +1000

    setting up Question and TemplateQuestionLink

---
+++ src/LifeTracker/EntityFramework/Models/Question.cs
@@ -0,0 +1,12 @@
+namespace LifeTracker.Models;
+
+public class Question
+{
+    public int Id { get; set; }
+
+    public required int QuestionNumber { get; set; }
+
+    public required string Text { get; set; }
+
+    public int TemplateQuestionLink { get; set; }
+}
---
+++ src/LifeTracker/EntityFramework/Models/TemplateQuestionLink.cs
@@ -0,0 +1,8 @@
+namespace LifeTracker.Models;
+
+public class TemplateQuestionLink
+{
+    public int TemplateId { get; set; }
+
+    public int QuestionId { get; set; }
+}

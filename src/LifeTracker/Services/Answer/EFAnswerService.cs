aae1b678412665a6020acded794be45246980a7d
Recelis <jackymlui@live.com>
2025-09-04 21:54:39 +1000

    create answer implementation + tests

---
+++ src/LifeTracker/Services/Answer/EFAnswerService.cs
@@ -0,0 +1,127 @@
+using LifeTracker.Data;
+using LifeTracker.Dto;
+using LifeTracker.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace LifeTracker.Services;
+
+public class EFAnswerService : IAnswerService
+{
+    private readonly LifeTrackerContext _lifeTrackerContext;
+    private readonly ILogger<EFAnswerService> _logger;
+
+    public EFAnswerService(LifeTrackerContext context, ILogger<EFAnswerService> logger)
+    {
+        _lifeTrackerContext = context;
+        _logger = logger;
+    }
+
+    // public async Task<List<Answer>> GetBySubmissionAsync(int templateId)
+    // {
+    //     return await _lifeTrackerContext.Template
+    //             .Where(t => t.Id == templateId)
+    //             .Include(t => t.TemplateAnswerLinks)
+    //             .ThenInclude(tql => tql.Answer)
+    //             .SelectMany(t => t.TemplateAnswerLinks.Select(tql => tql.Answer))
+    //             .ToListAsync();
+    // }
+
+    public async Task<Answer?> GetAsync(int answerId)
+    {
+        return await _lifeTrackerContext.Answer.FindAsync(answerId);
+    }
+
+    public async Task<Answer?> CreateAsync(CreateAnswerDto createAnswerDto)
+    {
+        // find the Submission and the Question
+        Submission? submission = await _lifeTrackerContext.Submission.FindAsync(createAnswerDto.SubmissionId);
+        if (submission == null)
+        {
+            _logger.LogError("No Submission of id {submissionId}", createAnswerDto.SubmissionId);
+            return null;
+        }
+
+        Question? question = await _lifeTrackerContext.Question.FindAsync(createAnswerDto.QuestionId);
+        if (question == null)
+        {
+            _logger.LogError("No Question of id {questionId}", createAnswerDto.QuestionId);
+            return null;
+        }
+
+        Answer newAnswer = new Answer
+        {
+            QuestionId = createAnswerDto.QuestionId,
+            SubmissionId = createAnswerDto.SubmissionId,
+            Text = createAnswerDto.Text,
+            Points = createAnswerDto.Points,
+        };
+
+        _lifeTrackerContext.Answer.Add(newAnswer);
+        await _lifeTrackerContext.SaveChangesAsync();
+
+        _logger.LogDebug("Created new Answer", newAnswer.ToString());
+
+        return newAnswer;
+    }
+
+    // public async Task<Answer?> UpdateAsync(int id, UpdateAnswerDto updateAnswerDto)
+    // {
+    //     try
+    //     {
+    //         Answer? answer = await _lifeTrackerContext.Answer.FindAsync(id);
+    //         if (answer == null)
+    //         {
+    //             _logger.LogError("Could not find answer of id {answer}", id);
+    //             return null;
+    //         }
+
+    //         answer.Text = updateAnswerDto.Text;
+    //         await _lifeTrackerContext.SaveChangesAsync();
+    //         return answer;
+    //     }
+    //     catch (Exception ex)
+    //     {
+    //         _logger.LogError(ex, "Answer Put exception");
+    //         return null;
+    //     }
+    // }
+
+    // /// <summary>
+    // /// Deletes Answer in its entirety
+    // /// </summary>
+    // /// <param name="answerId"></param>
+    // /// <returns></returns>
+    // public async Task<bool> DeleteAsync(int answerId)
+    // {
+    //     Answer? answer = await _lifeTrackerContext.Answer.FindAsync(answerId);
+    //     if (answer == null)
+    //     {
+    //         _logger.LogWarning("Answer could not be found");
+    //         return false;
+    //     }
+    //     else
+    //     {
+    //         using var transaction = await _lifeTrackerContext.Database.BeginTransactionAsync(System.Data.IsolationLevel.Serializable);
+
+    //         _lifeTrackerContext.Remove(answer);
+
+    //         await _lifeTrackerContext.SaveChangesAsync();
+    //         // remove template from Answer
+    //         int numDeleted = await _lifeTrackerContext.TemplateAnswerLink
+    //             .Where(tql => tql.AnswerId == answer.Id)
+    //             .ExecuteDeleteAsync();
+    //         await transaction.CommitAsync();
+
+    //         if (numDeleted == 0)
+    //         {
+    //             _logger.LogError("No templateAnswerLinks with {id} could be found", answer.Id);
+    //         }
+    //         else
+    //         {
+    //             _logger.LogInformation("Deleted {numDeleted} templateAnswerLinks", numDeleted);
+    //         }
+
+    //         return true;
+    //     }
+    // }
+}
---
+++ tests/LifeTrackerTests/AnswerTests.cs
@@ -0,0 +1,165 @@
+using NUnit.Framework;
+using LifeTracker.Services;
+using LifeTracker.Data;
+using LifeTracker.Dto;
+
+using Moq;
+using NUnit.Framework.Internal;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using LifeTracker.Models;
+using Microsoft.Data.Sqlite;
+
+namespace LifeTracker.Tests.Services
+{
+    [TestFixture]
+    public class AnswerTests
+    {
+        private SqliteConnection? _connection;
+        private IQuestionnaireService? _questionnaireService;
+        private ITemplateService? _templateService;
+        private ISubmissionService? _submissionService;
+        private IQuestionService? _questionService;
+        private IAnswerService? _answerService;
+
+        private LifeTrackerContext? _context;
+        private ILogger<EFQuestionnaireService>? _questionnaireLogger;
+        private ILogger<EFTemplateService>? _templateLogger;
+        private ILogger<EFSubmissionService>? _submissionLogger;
+        private ILogger<EFQuestionService>? _questionLogger;
+        private ILogger<EFAnswerService>? _answerLogger;
+
+        // Helper functions
+        private async Task<Questionnaire> CreateTestQuestionnaire(string name = "Test Questionnaire", string createdBy = "UnitTester")
+        {
+            var dto = new CreateQuestionnaireDto { Name = name, CreatedBy = createdBy };
+            return await _questionnaireService.CreateAsync(dto);
+        }
+
+        private async Task<Template> CreateTestTemplate(int questionnaireId, string name = "Test Template")
+        {
+            var dto = new CreateTemplateDto { Name = name, QuestionnaireId = questionnaireId };
+            return await _templateService!.CreateAsync(dto);
+        }
+
+        private async Task<Question> CreateTestQuestion(int templateId, string text = "test question")
+        {
+            var dto = new CreateQuestionDto { Text = text, TemplateId = templateId };
+            return await _questionService!.CreateAsync(dto);
+        }
+
+        private async Task<Submission> CreateTestSubmission(int templateId, string createdBy = "UnitTester")
+        {
+            var dto = new CreateSubmissionDto { TemplateId = templateId, CreatedBy = createdBy };
+            return await _submissionService!.CreateAsync(dto);
+        }
+
+        private async Task<Answer> CreateTestAnswer(CreateAnswerDto dto)
+        {
+            return await _answerService!.CreateAsync(dto);
+        }
+
+        [SetUp]
+        public void Setup()
+        {
+            _connection = new SqliteConnection("DataSource=:memory:");
+            _connection.Open();
+
+            var options = new DbContextOptionsBuilder<LifeTrackerContext>()
+                .UseSqlite(_connection)
+                .Options;
+            _context = new LifeTrackerContext(options);
+
+            _context.Database.EnsureDeleted();
+            _context.Database.EnsureCreated();
+
+            _questionnaireLogger = new Mock<ILogger<EFQuestionnaireService>>().Object;
+            _templateLogger = new Mock<ILogger<EFTemplateService>>().Object;
+            _questionLogger = new Mock<ILogger<EFQuestionService>>().Object;
+            _submissionLogger = new Mock<ILogger<EFSubmissionService>>().Object;
+            _answerLogger = new Mock<ILogger<EFAnswerService>>().Object;
+
+            _questionnaireService = new EFQuestionnaireService(_context, _questionnaireLogger);
+            _templateService = new EFTemplateService(_context, _templateLogger);
+            _questionService = new EFQuestionService(_context, _questionLogger);
+            _submissionService = new EFSubmissionService(_context, _submissionLogger);
+            _answerService = new EFAnswerService(_context, _answerLogger);
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            _context.Dispose();
+            _connection.Close();
+        }
+
+        [Test]
+        public async Task CreateAsync_ShouldAddAnswerToDb()
+        {
+            Questionnaire questionnaire = await CreateTestQuestionnaire();
+
+            Template template = await CreateTestTemplate(questionnaire.Id);
+
+            Question question = await CreateTestQuestion(template.Id);
+
+            Submission submission = await CreateTestSubmission(template.Id);
+
+            CreateAnswerDto createAnswerDto = new CreateAnswerDto
+            {
+                QuestionId = question.Id,
+                SubmissionId = submission.Id,
+                Text = "My Answer",
+                Points = 1
+            };
+
+            Answer answer = await CreateTestAnswer(createAnswerDto);
+
+            Assert.That(answer, Is.Not.Null);
+
+            Assert.That(createAnswerDto.SubmissionId, Is.EqualTo(answer.SubmissionId));
+            Assert.That(createAnswerDto.QuestionId, Is.EqualTo(answer.QuestionId));
+            Assert.That(createAnswerDto.Text, Is.EqualTo(answer.Text));
+            Assert.That(createAnswerDto.Points, Is.EqualTo(answer.Points));
+        }
+
+        // [Test]
+        // public async Task GetByUserAsync_ShouldGetOnlySubmissionAnswers()
+        // {
+        //     Questionnaire questionnaire = await CreateTestQuestionnaire();
+
+        //     Template template = await CreateTestTemplate(questionnaire.Id);
+
+        //     string CreatedBy0 = "UnitTester";
+        //     string CreatedBy1 = "NonUnitTester";
+
+        //     await CreateTestAnswer(template.Id, CreatedBy0);
+        //     await CreateTestAnswer(template.Id, CreatedBy0);
+        //     await CreateTestAnswer(template.Id, CreatedBy1);
+
+        //     List<Answer> answersInDb = await _answerService.GetByUserAsync(CreatedBy0);
+
+        //     Assert.That(answersInDb, Has.Exactly(2).Items);
+        // }
+
+        // [Test]
+        // public async Task DeleteAsync_ShouldDeleteAnswerToDb()
+        // {
+        //     Questionnaire questionnaire = await CreateTestQuestionnaire();
+
+        //     Template template = await CreateTestTemplate(questionnaire.Id);
+
+        //     CreateAnswerDto createAnswerDto = new CreateAnswerDto
+        //     {
+        //         TemplateId = template.Id
+        //     };
+
+        //     Answer answer = await CreateTestAnswer(template.Id);
+
+        //     await _answerService.DeleteAsync(answer.Id);
+
+        //     Answer answerInDb = await _context.Answer.FindAsync(answer.Id);
+
+        //     Assert.That(answerInDb, Is.Null);
+        // }
+    }
+}

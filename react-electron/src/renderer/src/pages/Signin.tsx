7a04cf6d8385fc9e1e014e069b03b448daf08e68
Recelis <jackymlui@live.com>
2025-11-11 07:08:31 +1000

    set up sign in form

--- react-electron/src/renderer/src/pages/Signin.tsx
+++ react-electron/src/renderer/src/pages/Signin.tsx
@@ -1,12 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router";
 import useAuth from "../hooks/useAuth";
 
 export default function Signin() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
   const auth = useAuth();
+
+  const onSubmit = () => {};
   return (
     <div>
       <Link to="/">Back</Link>
+      <form
+        onSubmit={onSubmit}
+        style={{ display: "flex", flexDirection: "column" }}
+      >
+        <h1>Sign in</h1>
+        <label htmlFor="email">Email</label>
+        <input
+          id="email"
+          type="text"
+          onChange={(e) => {
+            setEmail(e.target.value);
+          }}
+          value={email}
+        />
+
+        <label htmlFor="password">Password</label>
+        <input
+          id="password"
+          type="password"
+          onChange={(e) => {
+            setPassword(e.target.value);
+          }}
+          value={password}
+        />
+      </form>
     </div>
   );
 }
